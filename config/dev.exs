0096f6beedc19902487a3f9d1b7c96d843907d10
Max Groenlund <max@synthmax.dk>
2025-06-10 08:34:52 +0200

    Started on gen-cmds.txt

--- config/dev.exs
+++ config/dev.exs
@@ -8,7 +8,8 @@ config :clp_engine, CLP.Repo,
   database: "clp_engine_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
-  pool_size: 10
+  pool_size: 10,
+  port: 5433
 
 # For development, we disable any cache and enable
 # debugging and code reloading.
