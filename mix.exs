c91b68d8e3b3c62565ebe3ad355590c34a8923b5
Max Groenlund <max@synthmax.dk>
2025-09-14 07:20:10 +0200

    Bumped phoenix to 1.8.1

--- mix.exs
+++ mix.exs
@@ -26,7 +26,7 @@ defmodule SD.Umbrella.MixProject do
   # and cannot be accessed from applications inside the apps/ folder.
   defp deps do
     [
-      {:phoenix, "~> 1.8.0-rc.4", override: true},
+      {:phoenix, "~> 1.8.1", override: true},
       # Required to run "mix format" on ~H/.heex files from the umbrella root
       {:phoenix_live_view, ">= 0.0.0"}
     ]
